72d8be88bde90ba8ca3b7e959df16e2d825330f7
Andreas Kames <andreas_kames@yahoo.de>
2025-11-19 14:04:51 +0100

    Reigenfolge für Einbinden von dist-Ordner korrigiert

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 https://phonebook-6bmw.onrender.com/info
+https://phonebook-6bmw.onrender.com/api/persons
+
+http://localhost:3001
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
+app.use(express.static('dist'))
 var morgan = require('morgan')
 const cors = require('cors')
 
 app.use(cors())
 
-app.use(express.static('dist'))
 let persons =
 [
     { 
