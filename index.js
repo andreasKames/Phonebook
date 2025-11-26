5d7aaa39e6cee64daef18c1702a43871bb8b5e23
Andreas Kames <andreas_kames@yahoo.de>
2025-11-26 09:53:36 +0100

    Vor Exercise 3.13 Unnötigen Code gelöscht

--- index.js
+++ index.js
@@ -3,33 +3,7 @@ const app = express()
 app.use(express.json())
 app.use(express.static('dist'))
 var morgan = require('morgan')
-const cors = require('cors')
 
-app.use(cors())
-
-let persons =
-[
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    } 
-]
 
 const generateRandomId = () => {
   const maxId = 10000
